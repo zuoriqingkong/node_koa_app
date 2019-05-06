f1360f2a9327867fec9bf4b96171dc16eedbd840
youxubing <yxb9268@sina.com>
2019-05-06 17:09:36 +0800

    创建user表

---
+++ schema/user.js
@@ -0,0 +1,28 @@
+const moment = require('moment');
+
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('article', {
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncreament: true
+        },
+        nickname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            field: 'nickname'
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            field: 'phone'
+        },
+        createTime: {
+            type: DataTypes.DATE,
+            get(){
+                return moment(this.getDataValue('cerateTime').format('YYYY-MM-DD HH:mm:ss'))
+            }
+        }
+    });
+}
